4ea7b3fc1bba849bb0628ac343f574fd12b055d6
Vasiliy <Ruyk17@gmail.com>
2024-09-29 15:49:35 +0300

    Initial

---
+++ src/test/java/CalcOfRestServiceTest.java
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import ru.netology.services.CalcOfRestService;
+
+public class CalcOfRestServiceTest {
+
+    @Test
+    void shouldtestCalcOfRestService(
+    ) {
+        CalcOfRestService calcOfRestService = new CalcOfRestService();
+        int expected = 9;
+        int actual = calcOfRestService.calculate(10000, 3000);
+    }
+}
