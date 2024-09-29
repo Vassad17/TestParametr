6cb8a951f200d4f2829c7d8a3eb4f0498e035026
Vasiliy <Ruyk17@gmail.com>
2024-09-29 16:42:44 +0300

    ChangeEnd

--- src/test/java/CalcOfRestServiceTest.java
+++ src/test/java/CalcOfRestServiceTest.java
@@ -6,10 +6,16 @@ import ru.netology.services.CalcOfRestService;
 public class CalcOfRestServiceTest {
 
     @Test
-    void shouldtestCalcOfRestService(
-    ) {
+    void shouldtestCalcOfRestService() {
         CalcOfRestService calcOfRestService = new CalcOfRestService();
         int expected = 3;
-        int actual = calcOfRestService.calculate(10000, 3000);
+        int actual = calcOfRestService.calculate(10000, 3000, 20000);
+    }
+
+    @Test
+    void shouldtestCalcOfRestService2(){
+        CalcOfRestService calcOfRestService = new CalcOfRestService();
+        int expected = 2;
+        int actual = calcOfRestService.calculate(100_000, 60_000, 150_000);
     }
 }
